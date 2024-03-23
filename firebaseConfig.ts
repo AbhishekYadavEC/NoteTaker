a3a3b403f046cdb53c7880f690e42f7374d5fa0e
Abhishek Yadav <86556735+AbhishekYadavEC@users.noreply.github.com>
2024-03-23 18:36:13 +0530

    First Commit

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAVJ6S9IgLctcPfhKo3ShN-qYhMBTkHQiA",
+    authDomain: "codenear-ay.firebaseapp.com",
+    projectId: "codenear-ay",
+    storageBucket: "codenear-ay.appspot.com",
+    messagingSenderId: "648463676010",
+    appId: "1:648463676010:web:423af6d3db88b696709013"
+  };
